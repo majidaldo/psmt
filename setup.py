ba600f66624521922f6b1ec53cc7d712e50fd100
majidaldo <majidaldosar-github@yahoo.com>
2014-01-12 09:37:58 +0300

    simple installer

---
+++ setup.py
@@ -0,0 +1,12 @@
+
+
+from distutils.core import setup
+
+setup(name='psmt',
+      version='.1',
+      description='Paramater Study Management Tool',
+      author='Majid al-Dosari',
+      author_email='majid.aldosari on google\'s email',
+      url='https://github.com/majidaldo/psmt',
+      py_module=['analysisbase, fsdict,scriptmgt'],
+     )
